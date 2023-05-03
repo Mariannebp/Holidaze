5beaa4da73ffe601357906eaa59e704c566fe37a
Marianne Pedersen <mariannebpedersen@gmail.com>
2023-05-03 14:10:31 +0200

    Added functionality that checks if logged in and displays header content accordinagly

--- src/components/constants/index.jsx
+++ src/components/constants/index.jsx
@@ -3,3 +3,4 @@ export const baseUrl = "https://api.noroff.dev/api/v1/holidaze/";
 export const venuesUrl = baseUrl + "venues";
 export const registerUrl = baseUrl + "auth/register";
 export const loginUrl = baseUrl + "auth/login";
+export const profileUrl = baseUrl + "profiles/";
