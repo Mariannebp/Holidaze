d8312ee6dd0610fd5c9228ca7e6a581c5818666c
Marianne Pedersen <mariannebpedersen@gmail.com>
2023-05-05 18:45:55 +0200

    Created create new venue functionality

--- src/components/form/newVenueForm.jsx
+++ src/components/form/newVenueForm.jsx
@@ -3,10 +3,10 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Typography } from "@mui/material";
-import { red } from "@mui/material/colors";
-import * as g from "../../styles/global";
 import { venuesUrl } from "../constants";
 import { useNavigate } from "react-router-dom";
+import { red } from "@mui/material/colors";
+import * as g from "../../styles/global";
 
 const schema = yup
   .object({
@@ -14,10 +14,10 @@ const schema = yup
       .string()
       .min(3, "You need at least 3 characters")
       .max(50, "Your name must be 50 characters or less")
-      .required("Enter your name"),
+      .required("Enter a title for your venue"),
     description: yup
       .string()
-      .required("Enter your email address"),
+      .required("Provide a description of your venue"),
     media: yup
       .string()
       .matches(/^(http(s):\/\/.)[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/, "Enter a valid url")
@@ -29,24 +29,25 @@ const schema = yup
       .string()
       .required("Enter the country"),
     price: yup
-      .number("Enter a number")
+      .number()
+      .typeError("Enter a number")
       .required("Enter a price per night"),
     maxGuests: yup
-      .number("Enter a number")
+      .number()
+      .typeError("Enter a number")
+      .min(1, "Venue must room at least 1 guest")
+      .max(100, "Please enter number of guests 100 or less")
       .required("Enter maximum amount of guests"),
     meta: yup
       .boolean(),
-    location: yup
-      .string()
-      .required("Enter location"),
   })
-  .required();
+// .required();
 
 /**
  * Creates the form for new users to register an account, with validation
  */
 function NewVenueForm() {
-  const { register, formState: { errors }, } = useForm({ resolver: yupResolver(schema) });
+  const { register, handleSubmit, formState: { errors }, } = useForm({ resolver: yupResolver(schema) });
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [media, setMedia] = useState([]);
@@ -76,18 +77,12 @@ function NewVenueForm() {
    * Function that sends the information about the new venue to the api
    */
   async function onSubmit(e) {
-    e.preventDefault();
+    // e.preventDefault();
 
     const token = localStorage.getItem("token");
     const method = "post";
-    const venue = { name, description, media, price, maxGuests, meta }
-    // const venue = [
-    //   { name, description, media, price, maxGuests, meta },
-    //   { location }
-    // ]
+    const venue = { name, description, media, price, maxGuests, meta, location }
 
-
-    const venueInfo = { venue }
     const body = JSON.stringify(venue);
 
     const response = await fetch(venuesUrl, {
@@ -109,15 +104,10 @@ function NewVenueForm() {
     console.log(venue);
   }
 
-  // function onSubmit(e) {
+  // const handleMediaChange = (e) => {
   //   e.preventDefault();
-  //   console.log({ name: name, description: description, media: media, location: location, price: priceValue, maxGuests: maxGuestsValue, meta: meta })
+  //   setMedia((e) => setMedia(existMedia => [...existMedia, e.target.value]))
   // }
-
-  // const handleMediaChange = {(e) => {
-
-  // }}
-  // };
 
   const handleLocationChange = (e) => {
     setLocation({
@@ -135,7 +125,7 @@ function NewVenueForm() {
 
   return (
     <Box sx={{ margin: '30px 15px 50px 15px' }}>
-      <Box component="form" onSubmit={onSubmit}>
+      <Box component="form" onSubmit={handleSubmit(onSubmit)}>
         <div>
           <g.TextFieldMain
             fullWidth
@@ -167,10 +157,11 @@ function NewVenueForm() {
             type="url"
             value={media}
             {...register(`media`)}
-            onChange={(e) => setMedia(e.target.value)}
+            onChange={(e) => setMedia([e.target.value])}
           />
           <Typography variant="body2" sx={{ color: red.A700 }}>{errors.media?.message}</Typography>
         </div>
+
         <div>
           <g.TextFieldMain
             fullWidth
